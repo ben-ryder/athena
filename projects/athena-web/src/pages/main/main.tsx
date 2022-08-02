1b6548781239c465a9ad919238b5d959c5dc13bb
Ben-Ryder <dev@benryder.me>
2022-08-02 21:54:39 +0100

    wip: adding progress on main page layout/planning

--- projects/athena-web/src/pages/main/main.tsx
+++ projects/athena-web/src/pages/main/main.tsx
@@ -1,11 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import {IconButton, iconColorClassNames, iconSizes} from "@ben-ryder/jigsaw";
+import {IconButton, iconColorClassNames, iconSizes, MultiSelect} from "@ben-ryder/jigsaw";
 import {
-  User as AccountIcon,
   StickyNote as NotesIcon,
   Tag as TagsIcon,
   Filter as QueriesIcon,
-  ArrowLeft as BackIcon
+  ArrowLeft as BackIcon,
+  Trash2 as DeleteIcon,
+  Save as SaveIcon,
+  X as CloseIcon
 } from "lucide-react";
 import classNames from "classnames";
 import {useNavigate, useParams} from "react-router-dom";
@@ -14,14 +16,83 @@ import {Helmet} from "react-helmet-async";
 import {LoadingPage} from "../../patterns/pages/loading-page";
 import {useAthena} from "../../helpers/use-athena";
 import {GeneralQueryStatus} from "../../types/general-query-status";
-import {VaultDto} from "@ben-ryder/athena-js-lib";
+import {NoteContentDto, VaultDto} from "@ben-ryder/athena-js-lib";
+
+const notes: NoteContentDto[] = [
+  {
+    title: "test note 1",
+    body: "test note 1"
+  },
+  {
+    title: "test note 2",
+    body: "test note 2"
+  },
+  {
+    title: "test note 3",
+    body: "test note 3"
+  },
+  {
+    title: "test note 4",
+    body: "test note 5"
+  },
+  {
+    title: "test note 5",
+    body: "test note 5"
+  },
+  {
+    title: "test note 6",
+    body: "test note 6"
+  },
+  {
+    title: "test note 7",
+    body: "test note 7"
+  },
+  {
+    title: "test note 8",
+    body: "test note 8"
+  },
+  {
+    title: "test note 9",
+    body: "test note 9"
+  },
+  {
+    title: "test note 10",
+    body: "test note 10"
+  },
+]
+
+const tags = [
+  {
+    name: "tag 1",
+    value: "1"
+  },
+  {
+    name: "tag 2",
+    value: "2"
+  },
+  {
+    name: "tag 3",
+    value: "3"
+  },
+  {
+    name: "tag 4",
+    value: "4"
+  },
+  {
+    name: "tag 5",
+    value: "5"
+  }
+]
+
+const selectedTags = [tags[0], tags[3], tags[4]]
 
 /**
  * This height is used to ensure that the logo, vault details, vault switcher and the note header all line up.
  */
-const topSectionHeight = "h-[45px]";
+const topSectionHeight = "h-[40px]";
+const topSectionWidth = "w-[40px]";
 
-type VaultSections = "notes" | "queries" | "tags";
+type VaultSections = "notes" | "tasks" | "queries" | "tags";
 
 export function MainPage() {
   const navigate = useNavigate();
@@ -91,10 +162,10 @@ export function MainPage() {
         <div id="left-panel" className="min-h-[100vh] flex">
 
           {/** Vault Panel **/}
-          <section className="flex flex-col w-[500px] bg-br-atom-800">
+          <section className="flex flex-col w-[400px] bg-br-atom-900">
 
             {/** Vault Details **/}
-            <div className={`flex justify-center items-center relative ${topSectionHeight} border-b-2 border-br-blueGrey-700`}>
+            <div className={`flex justify-center items-center relative ${topSectionHeight}`}>
               <IconButton
                 label="Back to vaults"
                 icon={<BackIcon size={20} className={iconColorClassNames.secondary} />}
@@ -104,20 +175,10 @@ export function MainPage() {
                 className="absolute left-[0.5rem] py-2"
               />
               <p className="text-br-whiteGrey-100 font-bold py-2">{vault.name}</p>
-              <IconButton
-                label="Account Menu"
-                icon={
-                  <div className={iconColorClassNames.secondary + " flex items-center border-2 border-br-whiteGrey-100 hover:border-br-whiteGrey-200 rounded-[50%]"}>
-                    <AccountIcon size={20} className={iconColorClassNames.secondary} />
-                  </div>
-                }
-                onClick={() => {}}
-                className="absolute right-[0.5rem] py-2"
-              />
             </div>
 
             {/** Vault Panel Switcher **/}
-            <div className={`flex ${topSectionHeight} border-b-2 border-br-blueGrey-700`}>
+            <div className={`flex ${topSectionHeight}`}>
               <IconButton
                 label="Notes"
                 icon={
@@ -128,7 +189,7 @@ export function MainPage() {
                 }
                 onClick={() => {setCurrentVaultSection("notes")}}
                 className={classNames(
-                  "w-[33%] py-2",
+                  "grow py-2",
                   {
                     "stroke-br-whiteGrey-100 text-br-whiteGrey-200": currentVaultSection !== "notes",
                     "stroke-br-whiteGrey-100 text-br-whiteGrey-200 bg-br-teal-600": currentVaultSection === "notes"
@@ -145,7 +206,7 @@ export function MainPage() {
                 }
                 onClick={() => {setCurrentVaultSection("queries")}}
                 className={classNames(
-                  "w-[33%] py-2",
+                  "grow py-2",
                   {
                     "stroke-br-whiteGrey-100 text-br-whiteGrey-200": currentVaultSection !== "queries",
                     "stroke-br-whiteGrey-100 text-br-whiteGrey-200 bg-br-teal-600": currentVaultSection === "queries"
@@ -162,7 +223,7 @@ export function MainPage() {
                 }
                 onClick={() => {setCurrentVaultSection("tags")}}
                 className={classNames(
-                  "w-[33%] py-2",
+                  "grow py-2",
                   {
                     "stroke-br-whiteGrey-100 text-br-whiteGrey-200": currentVaultSection !== "tags",
                     "stroke-br-whiteGrey-100 text-br-whiteGrey-200 bg-br-teal-600": currentVaultSection === "tags"
@@ -173,21 +234,43 @@ export function MainPage() {
 
             {/** Vault Panel Content **/}
             <div className="">
+              {notes.map(note => (
+                <div key={note.title}>
+                  <button className="p-4 w-full text-br-whiteGrey-200 hover:bg-br-atom-600 text-left">{note.title}</button>
+                </div>
+              ))}
             </div>
           </section>
         </div>
 
-        <div id="main-panel w-full">
-          <section id="note-panel">
+        <div id="main-panel" className="w-full min-h-[100vh] flex flex-col">
+          <section id="note-details" className="bg-br-atom-800">
+            <div className={`${topSectionHeight} flex items-stretch`}>
+              <input className="grow block bg-transparent py-2 px-4 outline-none text-br-whiteGrey-100 font-bold" placeholder="note title here..." />
+              <IconButton label="Delete Note" icon={<DeleteIcon />} className={`${iconColorClassNames.secondary} h-full ${topSectionWidth} flex justify-center items-center`} onClick={() => {}} />
+              <IconButton label="Save Note" icon={<SaveIcon />} className={`${iconColorClassNames.secondary} h-full ${topSectionWidth} flex justify-center items-center`} onClick={() => {}} />
+            </div>
+            <div className={`${topSectionHeight}`}>
+              <MultiSelect
+                id="note-tags"
+                label="Note Tags"
+                hideLabel={true}
+                placeholder="add tags..."
+                options={tags}
+                currentOptions={selectedTags}
+                onOptionsChange={() => {}}
+                className="border-none bg-transparent"
+              />
+            </div>
+          </section>
+          <section id="note-content" className="grow">
 
           </section>
-          <section id="bottom-panel">
-
+          <section id="bottom-panel" className={`${topSectionHeight} bg-br-atom-800 flex justify-between items-center py-2 px-4`}>
+            <p className="text-br-whiteGrey-700 text-center italic">42 words | 526 chars</p>
+            <p className="text-br-whiteGrey-700 text-center italic">Online</p>
           </section>
         </div>
-
-        {/** Main Section **/}
-
       </div>
     </>
   );
