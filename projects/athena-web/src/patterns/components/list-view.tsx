b2b6f4e5b1a586e9f2428433d7311acc552121db
Ben-Ryder <dev@benryder.me>
2022-09-07 17:13:12 +0100

    wip: progress on tag view section

--- projects/athena-web/src/patterns/components/list-view.tsx
+++ projects/athena-web/src/patterns/components/list-view.tsx
@@ -1,24 +1,6 @@
 import {ContentList} from "./content-list";
-import {Accordion, Button, IconButton, iconColorClassNames, iconSizes, Input} from "@ben-ryder/jigsaw";
 import React from "react";
-import { Filter } from "lucide-react";
-import {FilterIcon} from "../element/filter-icon";
-import classNames from "classnames";
 import {ContentListFilterIconAndPopup} from "./popup-menus/content-list-filter-icon-and-popup";
-
-export function ListFilters() {
-  return (
-    <div className="mx-1">
-      <div>
-        <Input id="search" label="Search" hideLabel={true} placeholder="search content title..." />
-      </div>
-      <div className="flex justify-end">
-        <Button styling="secondary">Clear Filters</Button>
-        <Button>Apply Filters</Button>
-      </div>
-    </div>
-  )
-}
 
 
 export function ListView() {
--- projects/athena-web/src/patterns/components/tags-view.tsx
+++ projects/athena-web/src/patterns/components/tags-view.tsx
@@ -1,7 +1,36 @@
 import {TagForm} from "./tag-form";
+import {Button} from "@ben-ryder/jigsaw";
+import {useState} from "react";
+import {useAppDispatch} from "../../main/state/store";
+import {createTag} from "../../main/state/features/open-vault/tags/tags-actions";
+import {v4 as createUUID} from "uuid";
 
 export function TagsView() {
+  const dispatch = useAppDispatch();
+  const [showAddForm, setShowAddForm] = useState<boolean>(false);
+
   return (
-    <TagForm onSubmit={() => {}} onCancel={() => {}} submitText="Add New"/>
+    <>
+      <div>
+        <Button onClick={() => {setShowAddForm(true)}}>Add New</Button>
+      </div>
+      <div>
+        {showAddForm &&
+          <TagForm
+            onSubmit={(tagFields) => {dispatch(createTag({
+              id: createUUID(),
+              name: tagFields.name,
+              backgroundColour: tagFields.backgroundColour,
+              textColour: tagFields.textColour,
+              createdAt: new Date().toISOString(),
+              updatedAt: new Date().toISOString()
+            }))}}
+            onCancel={() => {setShowAddForm(false)}}
+            submitText="Add New"
+          />
+        }
+
+      </div>
+    </>
   )
 }
