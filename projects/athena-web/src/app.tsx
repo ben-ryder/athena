ce4e82dc7e8d2341d23749e577ef963a35abd133
Ben Ryder <dev@benryder.me>
2022-09-15 22:35:14 +0100

    fix: removing all routing & pages except main application

--- projects/athena-web/src/app.tsx
+++ projects/athena-web/src/app.tsx
@@ -1,30 +1,17 @@
-import React, {useState} from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
 import './styles/tailwind.css';
 
-import {HomePage} from "./pages/home";
 import {MainPage} from "./main/main";
-import {PageNotFound} from "./pages/page-not-found";
 import {Helmet, HelmetProvider} from "react-helmet-async";
 
 export function App() {
   return (
-      <HelmetProvider>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>Athena</title>
-        </Helmet>
-        <BrowserRouter>
-          <Routes>
-            {/* Home Route */}
-            <Route path={"/"} element={<HomePage />} />
-
-            <Route path="/app" element={<MainPage />} />
-
-            {/* 404 Route */}
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </HelmetProvider>
+    <HelmetProvider>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Athena</title>
+      </Helmet>
+      <MainPage />
+    </HelmetProvider>
   );
 }
--- projects/athena-web/src/routes.ts
+++
@@ -1,8 +0,0 @@
-
-export const routes = {
-  home: "/",
-  app: "/app",
-  external: {
-    github: "https://github.com/Ben-Ryder/athena"
-  }
-}
