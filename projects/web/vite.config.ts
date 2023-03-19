d8d3d5466e0bc8de6e130fcab40eec764aebe609
Ben Ryder <dev@benryder.me>
2023-03-19 22:44:30 +0000

    feat: adding intial skeleton web package with PWA plugin

---
+++ projects/web/vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Athena',
+        short_name: 'Athena',
+        description: 'A local-first web app for notes, tasks, journaling, habit tracking and reminders.',
+        theme_color: '#0c857a',
+        icons: [
+          {
+            src: '/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512-maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
+})
