310e5a8d11b06610a55759dd28811985a8736eaf
Ben-Ryder <dev@benryder.me>
2023-05-24 18:38:39 +0100

    wip: progress on global layout & menu bar

--- projects/web/src/patterns/layout/global-layout/global-layout.tsx
+++ projects/web/src/patterns/layout/global-layout/global-layout.tsx
@@ -1,8 +1,28 @@
 import React, {ReactNode} from 'react';
 import {Helmet} from "react-helmet-async";
-import {Header} from "../header/header";
+import {
+  File as NotesIcon,
+  ListChecks as TasksIcon,
+  Tag as TagsIcon,
+  CalendarRange as JournalIcon,
+  MoreHorizontal as MobileMenuIcon
+} from "lucide-react";
+import {routes} from "../../../routes";
+
 export interface GlobalLayoutProps {
   children: ReactNode
+}
+
+export function AccountIcon() {
+  // this is the lucide-react UserCircle2 icon, but it doesn't appear in the react package
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+         stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-user-circle-2">
+      <path d="M18 20a6 6 0 0 0-12 0"></path>
+      <circle cx="12" cy="10" r="4"></circle>
+      <circle cx="12" cy="12" r="10"></circle>
+    </svg>
+  )
 }
 
 export function GlobalLayout(props: GlobalLayoutProps) {
@@ -13,11 +33,37 @@ export function GlobalLayout(props: GlobalLayoutProps) {
         <title>{`Application | Athena`}</title>
       </Helmet>
 
-      <Header />
-
-      <main className="ath-main">
+      <div className="ath-layout">
+        <div className="ath-menu-bar">
+          <div className="ath-menu-bar__logo">
+            <span className="logo"></span>
+            <p>Athena</p>
+          </div>
+          <div className="ath-menu-bar__menu">
+            <a href={routes.content.notes.list} className="ath-menu-bar__menu-link">
+              <NotesIcon />
+            </a>
+            <a href={routes.content.tasks.list} className="ath-menu-bar__menu-link">
+              <TasksIcon />
+            </a>
+            <a href={routes.content.journal.list} className="ath-menu-bar__menu-link">
+              <JournalIcon />
+            </a>
+            <a href={routes.organisation.tags.list} className="ath-menu-bar__menu-link">
+              <MobileMenuIcon />
+            </a>
+            {/*<a href={routes.organisation.tags.list} className="ath-menu-bar__menu-link">*/}
+            {/*  <TagsIcon />*/}
+            {/*</a>*/}
+            {/*<a href={routes.organisation.tags.list} className="ath-menu-bar__menu-link ath-menu-bar__menu-link--account">*/}
+            {/*  <AccountIcon />*/}
+            {/*</a>*/}
+          </div>
+        </div>
+        <main className="ath-main">
           {props.children}
-      </main>
+        </main>
+      </div>
     </div>
   )
 }
