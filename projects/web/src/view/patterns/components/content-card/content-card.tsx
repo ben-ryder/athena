51e192757e0280928044237c00d3572a3caec1d7
Ben Ryder <dev@benryder.me>
2024-04-24 09:05:32 +0100

    fix: make content card tabbable

--- projects/web/src/view/patterns/components/content-card/content-card.tsx
+++ projects/web/src/view/patterns/components/content-card/content-card.tsx
@@ -12,7 +12,7 @@ export interface ContentCardProps {
 
 export function ContentCard(props: ContentCardProps) {
   return (
-    <div className="content-card" onClick={props.onSelect} tabIndex={1}>
+    <div className="content-card" onClick={props.onSelect} tabIndex={0}>
       <div className="content-card__content">
         <h3 className="content-card__name">{props.name}</h3>
         {props.description && (
