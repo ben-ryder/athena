b1cece86be3d66bcbd0c0bcf501b748e2536ba01
Ben Ryder <dev@benryder.me>
2024-07-01 23:28:00 +0100

    feat: adding initial LocalfulEncryption functionality

--- projects/web/localful/encryption/localful-encryption.ts
+++ projects/web/localful/encryption/localful-encryption.ts
@@ -1,35 +1,40 @@
-import { ActionResult, ErrorTypes } from "../control-flow";
 import { z } from "zod";
-import * as WebCrypto from "easy-web-crypto"
-import {Buffer} from 'buffer'
+import { Buffer } from "buffer";
+
+export const UnlockKeyMetadata = z.object({
+	algo: z.literal("PBKDF2"),
+	salt: z.string(),
+	iterations: z.literal(100000),
+	hash: z.literal("SHA-256"),
+})
+export type UnlockKeyMetadata = z.infer<typeof UnlockKeyMetadata>
+
+export interface UnlockKey {
+	key: CryptoKey
+	metadata: UnlockKeyMetadata
+}
 
 export interface CreatedEncryptionKey {
 	protectedEncryptionKey: string
-	encryptionKey: CryptoKey
+	encryptionKey: string
 }
 
+export const EncryptionMetadata = z.object({
+	algo: z.literal("AES-GCM"),
+	iv: z.string(),
+})
+export type EncryptionMetadata = z.infer<typeof EncryptionMetadata>
 
-const INITIALIZATION_VECTOR_LENGTH = 12;
-const SALT_LENGTH = 16;
-
-/**
- * A helper class which contains the cryptographic functions used
- * to encrypt and decrypt data.
- */
 export class LocalfulEncryption {
 
-	static async createDatabaseEncryptionKey(password: string): Promise<CreatedEncryptionKey> {
-		const encryptionKey = await window.crypto.subtle.generateKey(
-			{
-				name: "AES-GCM",
-				length: 256,
-			},
-			true,
-			["encrypt", "decrypt"],
-		);
+	static generateUUID(): string {
+		return self.crypto.randomUUID()
+	}
 
-		const {key: unlockKey, salt: unlockKeySalt } = await LocalfulEncryption.deriveUnlockKey(password)
-		const protectedEncryptionKey = await LocalfulEncryption.wrapEncryptionKey(encryptionKey, unlockKey, unlockKeySalt)
+	static async createDatabaseEncryptionKey(password: string): Promise<CreatedEncryptionKey> {
+		const encryptionKey = await LocalfulEncryption._createEncryptionKey()
+		const unlockKey = await LocalfulEncryption._deriveUnlockKey(password)
+		const protectedEncryptionKey = await LocalfulEncryption._wrapEncryptionKey(encryptionKey, unlockKey)
 
 		return {
 			encryptionKey,
@@ -37,49 +42,71 @@ export class LocalfulEncryption {
 		}
 	}
 
-	static async decryptDatabaseEncryptionKey(protectedEncryptionKey: string, password: string): Promise<CryptoKey> {
-		const [version, metadata, wrappedKey] = protectedEncryptionKey.split(':')
-		if (version !== 'v1' || !metadata || !wrappedKey) {
-			throw new Error('protectedEncryptionKey is not in correct format')
+	static async decryptDatabaseEncryptionKey(protectedEncryptionKey: string, password: string): Promise<string> {
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars -- version may be used in future
+		const [version, encodedMetadata, encodedEncryptionKey] = protectedEncryptionKey.split(".")
+
+		const decodedMetadata = Buffer.from(encodedMetadata, "base64").toString()
+		const unlockKeyMetadata = UnlockKeyMetadata.parse(JSON.parse(decodedMetadata))
+		const unlockKey = await LocalfulEncryption._deriveUnlockKey(password, unlockKeyMetadata.salt)
+
+		const decodedEncryptionKey = Buffer.from(encodedEncryptionKey, "base64").toString()
+
+		return await LocalfulEncryption._decryptWithKey(unlockKey.key, decodedEncryptionKey)
+	}
+
+	static async encrypt<T>(encryptionKey: string, data: T): Promise<string> {
+		const key = await LocalfulEncryption._getEncryptionCryptoKey(encryptionKey)
+		return LocalfulEncryption._encryptWithKey(key, data)
+	}
+
+	static async decrypt<T>(
+		encryptionKey: string,
+		ciphertext: string,
+		validationSchema?: z.ZodType<T>,
+	): Promise<T> {
+		const key = await LocalfulEncryption._getEncryptionCryptoKey(encryptionKey)
+		const result = await LocalfulEncryption._decryptWithKey(key, ciphertext)
+
+		if (!validationSchema) {
+			return result as T
 		}
+		return validationSchema.parse(result)
+	}
 
-		const decodedMetadata = JSON.parse(Buffer.from(metadata, 'base64').toString())
-		const {key: unlockKey} = await LocalfulEncryption.deriveUnlockKey(password, decodedMetadata.salt)
+	static async _createEncryptionKey(): Promise<string> {
+		const rawKey = window.crypto.getRandomValues(new Uint8Array(32));
+		const encodedKey = Buffer.from(rawKey).toString("base64");
+		return `v1.${encodedKey}`
+	}
 
-		const wrappedEncruptionKey = Buffer.from(wrappedKey, 'base64').buffer
-		const iv = Buffer.from(decodedMetadata.iv, 'base64').buffer
+	private static _getEncryptionCryptoKey(encryptionKeyString: string): Promise<CryptoKey> {
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars -- version isn't used, but may be needed in the future.
+		const [version, encodedKeyMaterial] = encryptionKeyString.split('.')
+		const keyMaterial = Buffer.from(encodedKeyMaterial, 'base64').buffer
 
-		const unWrappedKey = await window.crypto.subtle.unwrapKey(
+		return window.crypto.subtle.importKey(
 			"raw",
-			wrappedEncruptionKey,
-			unlockKey,
+			keyMaterial,
 			{
 				name: "AES-GCM",
-				iv
-			},
-			{
-				name: "AES-GCM"
 			},
 			false,
 			["encrypt", "decrypt"]
 		)
-
-		return unWrappedKey
 	}
 
-	static async deriveUnlockKey(password: string, derivationSalt?: string): Promise<{ key: CryptoKey, salt: string }> {
-		const encoder = new TextEncoder();
-
+	private static async _deriveUnlockKey(password: string, derivationSalt?: string): Promise<UnlockKey> {
 		const baseKey = await window.crypto.subtle.importKey(
 			"raw",
-			encoder.encode(password),
+			Buffer.from(password),
 			"PBKDF2",
 			false,
 			["deriveKey", "deriveBits"]
 		)
 		const salt = derivationSalt
-			? Buffer.from(derivationSalt, 'base64').buffer
-			: window.crypto.getRandomValues(new Uint8Array(SALT_LENGTH));
+			? Buffer.from(derivationSalt, 'base64')
+			: window.crypto.getRandomValues(new Uint8Array(16));
 
 		const derivedKey = await window.crypto.subtle.deriveKey(
 			{
@@ -91,141 +118,78 @@ export class LocalfulEncryption {
 			baseKey,
 			{ 'name': 'AES-GCM', 'length': 256 },
 			true,
-			["wrapKey", "unwrapKey"]
+			// Although this key used as a KEK, the wrapping is done
+			// manually via the _encryptWithKey and _decryptWithKey methods
+			// which means this key usage is actually encrypt/decrypt not wrapKey/unwrapKey.
+			["encrypt", "decrypt"]
 		)
 
 		const encodedSalt = Buffer.from(salt).toString('base64')
+
 		return {
 			key: derivedKey,
-			salt: encodedSalt
+			metadata: {
+				algo: "PBKDF2",
+				salt: encodedSalt,
+				iterations: 100000,
+				hash: "SHA-256"
+			}
 		}
 	}
 
-	static async wrapEncryptionKey(encryptionKey: CryptoKey, unlockKey: CryptoKey, unlockKeySalt: string): Promise<string> {
-		const iv = window.crypto.getRandomValues(new Uint8Array(INITIALIZATION_VECTOR_LENGTH));
+	private static async _wrapEncryptionKey(encryptionKey: string, unlockKey: UnlockKey): Promise<string> {
+		const wrappedKey = await LocalfulEncryption._encryptWithKey(unlockKey.key, encryptionKey)
 
-		const wrappedKey = await window.crypto.subtle.wrapKey(
-			"raw",
-			encryptionKey,
-			unlockKey,
+		const encodedMetadata = Buffer.from(JSON.stringify(unlockKey.metadata)).toString('base64')
+		const encodedKey = Buffer.from(wrappedKey).toString('base64')
+
+		return `v1.${encodedMetadata}.${encodedKey}`
+	}
+
+	private static async _encryptWithKey<T>(key: CryptoKey, data: T): Promise<string> {
+		const iv = window.crypto.getRandomValues(new Uint8Array(12));
+
+		const encryptedData = await window.crypto.subtle.encrypt(
 			{
 				name: "AES-GCM",
 				iv
-			}
+			},
+			key,
+			// todo: improve generic types to narrow allowed data?
+			Buffer.from(JSON.stringify(data))
 		)
+		const encodedData = Buffer.from(encryptedData).toString('base64')
 
-		const metadata = {
+		const metadata: EncryptionMetadata = {
+			algo: "AES-GCM",
 			iv: Buffer.from(iv).toString('base64'),
-			salt: unlockKeySalt
 		}
 		const encodedMetadata = Buffer.from(JSON.stringify(metadata)).toString('base64')
-		const encodedKey = Buffer.from(wrappedKey).toString('base64')
 
-		// Export the key (and iv value used for encryption) in a raw base64 encoded string suitable for saving anywhere.
-		// The version is stored in order to handle future data migrations should the method of encryption change.
-		return `v1:${encodedMetadata}:${encodedKey}`
+		return `v1.${encodedMetadata}.${encodedData}`
 	}
 
-	/**
-   * Encrypt the given text.
-   * The returned string will include the encrypted text and initialization vector combined.
-   *
-   * @param key
-   * @param text
-   * @throws EncryptError
-   */
-	static async encryptText(key: CryptoKey, text: string): Promise<ActionResult<string>> {
-		const cipherData = await WebCrypto.encrypt(key, text)
-		const ciphertext = JSON.stringify(cipherData)
-
-		return {
-			success: true,
-			data: ciphertext
-		}
-	}
-
-	/**
-   * Decrypt the given text.
-   * The passed string should include the encrypted text, salt and initialization vector.
-   *
-   * @param key
-   * @param text
-   * @throws DecryptError
-   */
-	static async decryptText(key: CryptoKey, text: string): Promise<ActionResult<string>> {
-		const data = JSON.parse(text)
-		const result = await WebCrypto.decrypt(key, data)
-
-		return {
-			success: true,
-			data: result
-		}
-	}
-
-	/**
-   * Encrypt the given data.
-   *
-   * @param key
-   * @param data
-   */
-	static async encryptData<T>(key: CryptoKey, data: T): Promise<ActionResult<string>> {
-		const dataString = JSON.stringify(data)
-		return LocalfulEncryption.encryptText(key, dataString)
-	}
-
-	/**
-   * Decrypt and validate the given data.
-   *
-   * @param key
-   * @param schema
-   * @param ciphertext
-   */
-	static async decryptAndValidateData<T>(
+	private static async _decryptWithKey(
 		key: CryptoKey,
-		schema: z.ZodType<T>,
-		ciphertext: string
-	): Promise<ActionResult<T>> {
-		const decryptResult = await LocalfulEncryption.decryptText(key, ciphertext)
-		if (!decryptResult.success) return decryptResult
+		ciphertext: string,
+	): Promise<string> {
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars -- may be used in future
+		const [version, encodedMetadata, encodedData] = ciphertext.split(".")
 
-		let rawData
-		try {
-			rawData = JSON.parse(decryptResult.data)
-		}
-		catch (e) {
-			return {
-				success: false,
-				errors: [{
-					type: ErrorTypes.INVALID_OR_CORRUPTED_DATA,
-					devMessage: "attempted to decrypt data that could not be parsed"
-				}]
-			}
-		}
+		const decodedMetadata = Buffer.from(encodedMetadata, "base64").toString()
+		const metadata = EncryptionMetadata.parse(JSON.parse(decodedMetadata))
 
-		const parseResult = schema.safeParse(rawData)
-		if (parseResult.success) {
-			return {
-				success: true,
-				data: parseResult.data
-			}
-		}
+		const decodedCiphertext = Buffer.from(encodedData, "base64")
 
-		return {
-			success: false,
-			errors: [{
-				type: ErrorTypes.INVALID_OR_CORRUPTED_DATA,
-				devMessage: "decrypted data failed validation",
-				context: parseResult.error
-			}]
-		}
-	}
+		const decryptedData = await window.crypto.subtle.decrypt(
+			{
+				name: "AES-GCM",
+				iv: Buffer.from(metadata.iv, "base64"),
+			},
+			key,
+			decodedCiphertext
+		)
 
-	/**
-   * Generate a random UUID value.
-   */
-	static generateUUID(): string {
-		return self.crypto.randomUUID()
+		return JSON.parse(Buffer.from(decryptedData).toString())
 	}
 }
-
-window.LocalfulEncryption = LocalfulEncryption
