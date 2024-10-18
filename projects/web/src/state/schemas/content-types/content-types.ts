3b8e0e3949d8022120c44c1b94807f33c7151548
Ben Ryder <dev@benryder.me>
2024-10-18 19:55:03 +0100

    fix: removing template description from content type

--- projects/web/src/state/schemas/content-types/content-types.ts
+++ projects/web/src/state/schemas/content-types/content-types.ts
@@ -9,7 +9,6 @@ export const ContentTypeData = z.object({
 	icon: z.string().optional(),
 	colourVariant: ColourVariants.optional(),
 	contentTemplateName: NameField.optional(),
-	contentTemplateDescription: NameField.optional(),
 	contentTemplateTags: z.array(IdField).optional(),
 	fields: z.array(IdField)
 }).strict()
